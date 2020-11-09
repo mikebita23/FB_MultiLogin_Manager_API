c7d4d9e7521176282c4901a85282b23828bf3d5b
Nidal El Fengaoui <nidal.fengaoui@gmail.com>
2020-11-09 10:31:44 +0100

    Initializing empty project

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// IMPORTS 
+require('dotenv/config');
+const Express = require('express');
+const BodyParsser = require('body-parser');
+const Routes = require('./Routes/Routes');
+
+// INSTANCING THE SERVER
+const app = Express();
+
+// MIDDLEWARES
+app.use(BodyParsser.json());
+
+//ROUTES
+app.get('/', (req, res)=> { res.send('The app is On') });
+
+// START LISTENING
+app.listen(process.env.LISTEN_PORT || 3003);
