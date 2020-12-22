1c705cd356c2640a19ef471d138897f1ff35e6f0
Nidal El Fengaoui <nidal.fengaoui@gmail.com>
2020-12-22 15:12:10 +0100

    Update Proxy behavior

--- Routes/Proxy.js
+++ Routes/Proxy.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const Controller = require(__controllers + 'proxy.controller');
 const CheckAuthMiddleWare= require(__middleWares + 'userMiddleWare')
 
-router.get('/open', Controller.createProxy);
-router.get('/close/:port', Controller.closeProxy);
-router.get('/get',Controller.getOpenPorts)
+router.get('/get',CheckAuthMiddleWare, Controller.getProxy);
 
 module.exports = router;
--- controllers/proxy.controller.js
+++ controllers/proxy.controller.js
@@ -1,46 +1,38 @@
-const ProxyChain = require('proxy-chain');
-const proxyChain = require('proxy-chain');
-const Hlp = require(__helpers + 'userHelpers')
+const rp = require('require-promise');
+const Hlp = require(__helpers + 'userHelpers');
 
-const luminatiProxyUrl = process.env.LUMINATI_PROXY
-//  || "http://lum-customer-williamsellier-zone-residential20:lts3a949g9hw@zproxy.lum-superproxy.io:22225";
+const lumUser = process.env.LUMINATI_USER
+const lumZone = process.env.LUMINATI_ZONE
+const lumPort = process.env.LUMINATI_PORT || 22225
+const lumTkn = process.env.LUMINATI_KEY || '2d79281c3f4fa213797f3730d77feaa5'
+
+setUrl = ()=>{
+    
+}
+
 
 module.exports = {
-    createProxy: (req, res) => {
 
-        proxyChain.anonymizeProxy(luminatiProxyUrl).then(prx => {
-            __proxyPorts.push(prx.split(':')[2]); __proxyCount++;
-            res.status(200).json({
-                proxy: prx
-            })
-        })
-    
-    },
-    closeProxy: (req, res) =>{
-        if(!Hlp.hasAllParams(req.params, ['port'])){
-            return res.status(400).json({
-                message: "BAD REQUEST: 'port' not found !"
-            })
-        }
-
-        let index = __proxyPorts.indexOf(req.params.port)
-        if(index !== -1) {
-            __proxyPorts.splice(index, 1);
-            __proxyCount-- ;
-            ProxyChain.closeAnonymizedProxy(req.params.port, true);
-            return res.status(200).json({
-                message: "Proxy closed successfully !"
-            })
-        }
-
-        res.status(401).json({
-            message: "Proxy not found !"
-        })
-    },
-    getOpenPorts: (req, res)=>{
-        res.status(200).json({
-            count: __proxyCount,
-            ports: __proxyPorts
-        })
+    getProxy: (req, res)=>{
+        if(lumUser && lumPwd && lumZone)
+            rp({
+                url: `https://luminati.io/api/zone/passwords?zone=${lumZone}`,
+                headers: {'Authorization': 'Bearer '}
+            }).then( res => {
+                passwords = JSON.parse(data)
+                if(passwords.length > 0)
+                    res.status(200).json({
+                        url: `http://lum-customer-${lumUser}-zone-${lumZone}:${passwords[0]}@zproxy.lum-superproxy.io:${lumPort}`
+                    });
+            }).cath( err =>{
+                res.status(500).json({
+                    message: "INTERNEL ERROR : something wrong with porxy"
+                });
+            });
+            
+        else 
+            res.status(500).json({
+                message: "INTERNEL ERROR : something wrong with porxy"
+            });
     }
 }
