f5470ef1a4aeb8f0e34efe1a299bd5f1cb72ec34
Nidal El Fengaoui <nidal.fengaoui@gmail.com>
2020-12-22 15:25:03 +0100

    Fix Auth Middlware Error

--- middleWares/userMiddleWare.js
+++ middleWares/userMiddleWare.js
@@ -1,12 +1,13 @@
 const jwt = require("jsonwebtoken")
 
 function checkAuth(req, res, next) {
-    const Token = req.headers.authorization.split(" ")[1];
-    const decodedToken = require(__helpers + 'userHelpers').decodeToken(Token)
-    if(decodedToken){
-        req.userData = decodedToken;
-        if(req.userData.exp > new Date().getTime() / 1000 )
-            next();
+    if(req.headers.authorization){
+        const Token = req.headers.authorization.split(" ")[1];
+        const decodedToken = require(__helpers + 'userHelpers').decodeToken(Token)
+        if(decodedToken){
+            req.userData = decodedToken;
+            if(req.userData.exp > new Date().getTime() / 1000 )
+                next();
     }else{
         return res.status(401).json({
             message: "Invalide or expired token",
