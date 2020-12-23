fe68d030e730be4d49d85421f900a0838306981c
Nidal El Fengaoui <nidal.fengaoui@gmail.com>
2020-12-23 11:54:17 +0100

    Patch Proxy Password Bug

--- controllers/proxy.controller.js
+++ controllers/proxy.controller.js
@@ -1,34 +1,40 @@
-const rp = require('require-promise');
-const Hlp = require(__helpers + 'userHelpers');
+const rp = require('request-promise')
 
-const lumUser = process.env.LUMINATI_USER
-const lumZone = process.env.LUMINATI_ZONE
+const lumUser = process.env.LUMINATI_USER || "c_fa52977c"
+const lumZone = process.env.LUMINATI_ZONE || "residential20"
 const lumPort = process.env.LUMINATI_PORT || 22225
 const lumTkn = process.env.LUMINATI_KEY || '2d79281c3f4fa213797f3730d77feaa5'
-
 
 module.exports = {
 
     getProxy: (req, res)=>{
-        if(lumUser && lumPwd && lumZone)
+        console.log("I'm called");
+        if(lumUser && lumZone){
             rp({
                 url: `https://luminati.io/api/zone/passwords?zone=${lumZone}`,
                 headers: {'Authorization': 'Bearer '+lumTkn}
-            }).then( res => {
-                passwords = JSON.parse(data)
-                if(passwords.length > 0)
+            }).then( data => {
+                console.log('we got a response: ', data);
+                passwords = JSON.parse(data).passwords
+                console.log(passwords);
+                if(passwords.length > 0){
+                    console.log('responding now');
                     res.status(200).json({
                         url: `http://lum-customer-${lumUser}-zone-${lumZone}:${passwords[0]}@zproxy.lum-superproxy.io:${lumPort}`
                     });
-            }).cath( err =>{
+                }
+            }).catch( err =>{
+                console.log('err in calling luminati');
                 res.status(500).json({
                     message: "INTERNEL ERROR : something wrong with porxy"
                 });
             });
-            
-        else 
+        }
+        else {
+            console.log('user and zone doesnt exist');
             res.status(500).json({
                 message: "INTERNEL ERROR : something wrong with porxy"
             });
+        }
     }
 }
--- middleWares/userMiddleWare.js
+++ middleWares/userMiddleWare.js
@@ -1,18 +1,22 @@
 const jwt = require("jsonwebtoken")
 
 function checkAuth(req, res, next) {
-    if(req.headers.authorization){
-        const Token = req.headers.authorization.split(" ")[1];
-        const decodedToken = require(__helpers + 'userHelpers').decodeToken(Token)
-        if(decodedToken){
-            req.userData = decodedToken;
-            if(req.userData.exp > new Date().getTime() / 1000 )
-                next();
-        }
+    
+    if(typeof req.headers.authorization == 'undefined'){
+        return res.status(401).json({
+            message: "Unauthorized .... Bearer Token Needed !",
+        })
+    }
+    const Token = req.headers.authorization.split(" ")[1];
+    const decodedToken = require(__helpers + 'userHelpers').decodeToken(Token)
+
+    if(decodedToken){
+        req.userData = decodedToken;
+        if(req.userData.exp > new Date().getTime() / 1000 )
+            next();
     }else{
         return res.status(401).json({
             message: "Invalide or expired token",
-            error: error
         })
     }
 }
