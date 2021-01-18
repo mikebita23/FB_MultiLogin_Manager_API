471c84e4c57507e2c16383234181d66947bd0ee1
GLS 02 <traoreibrahima2708>
2021-01-18 14:43:52 +0100

    Ajout Edit Forfait

--- controllers/forfait.controller.js
+++ controllers/forfait.controller.js
@@ -100,6 +100,60 @@ function addForfait(req, res){
         })
     }
 }
+function updateForf(req, res) {
+
+    // checking wich params should be update and fetching it values 
+    let paramsToUpdate = userHlp.wichParams(req.body, ['id', 'nom','description','prix'])
+    let newValues = userHlp.fetchAttrFromRequest(req.body, paramsToUpdate)
+
+    // no param verification
+    if (paramsToUpdate.length == 0) return res.status(400).json({ message: "BAD REQUEST: no parameters! to update" });
+    
+    // seting up the account to update
+    let id = (paramsToUpdate.indexOf('id') >= 0 && req.userData.isAdmin) ? newValues.id : req.userData.userId;
+    delete newValues.id
+
+    //recovering old data
+    Models.Forfait.findOne({
+        where: {id: id}
+    }).then(oldForfait => {
+
+        // checking if user really exist
+        if(oldForfait){
+            
+            //verifying if new email is used by someone else
+            if(paramsToUpdate.indexOf('email') >= 0 ){
+                Models.Forfait.findOne({
+                    where: {
+                        nom: newValues.nom,
+                        id: {
+                            [Op.ne] : id
+                        }
+                    }
+                }).then(forf=> { if(forf) return res.status(401).json({ message: 'nom already exist ' })
+                }).catch(err => { res.status(500).json({ message: "Something Went Wrong !!!", error: err })                })
+            }
+
+            // if new passWord it should be hashed
+            if(paramsToUpdate.indexOf('prix') >= 0 ){
+              
+                    //newValues.passWord = hash
+                    Models.Forfait.update(newValues, { where: { id: id } })
+                    .then(result => { return res.status(200).json(result)})
+                    .catch(err => { return res.status(500).json({message: "Something went wrong", error: err})});        
+               
+            }else {
+                Models.Forfait.update(newValues, { where: { id: id } })
+                .then(result => { return res.status(200).json(result)})
+                .catch(err => { return res.status(500).json({message: "Something went wrong", error: err})});
+            }               
+
+        }else {
+            return res.status(404).json({ message: "USER NOT FOUND !!" });
+        }
+    }).catch(err => { res.status(500).json({ message: "-Something Went Worong !!!", error: err }) })
+
+}
 
 function updateForfait(req, res) {
 
