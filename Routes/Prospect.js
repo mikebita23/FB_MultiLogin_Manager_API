da855b8b7b17e22c8bc7673b7ddc024200c12e26
GLS 02 <traoreibrahima2708>
2021-02-01 11:33:00 +0100

    Ajout isAdmin dans le CRD prospect

--- Routes/Prospect.js
+++ Routes/Prospect.js
@@ -1,11 +1,20 @@
 const express= require('express');
 const router =express.Router();
 const Controller = require('../controllers/prospect.controller');
+const CheckAuthMiddleWare= require(__middleWares + 'userMiddleWare')
 
-router.get('/all', Controller.getProspects);
-router.get('/:id', Controller.getProspect);
+function isAdminMiddlware(req, res, next){
+    if(!req.userData.isAdmin)
+    res.status(401).json({
+        message: 'Unauthorized Request !'
+    })
+    else next()
+}
+
+router.get('/all', CheckAuthMiddleWare.checkAuth,isAdminMiddlware, Controller.getProspects);
+router.get('/:id',CheckAuthMiddleWare.checkAuth,isAdminMiddlware, Controller.getProspect);
 router.post('/add', Controller.addProspect);
-router.get('/del/:id', Controller.deleteProspect);
+router.get('/del/:id',CheckAuthMiddleWare.checkAuth,isAdminMiddlware, Controller.deleteProspect);
 
 
 
